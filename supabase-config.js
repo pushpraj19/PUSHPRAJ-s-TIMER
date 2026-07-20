8b8bb1bd63f615df27c58195008ff5022e07f004
pushpraj19 <pushprajbudha19@gmail.com>
2026-07-20 06:12:23 +0545

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-// Paste your Supabase Project URL and publishable (or anon) key below.
-// This key is safe to use in the browser when the SQL security rules are enabled.
-window.FOCUS_SUPABASE_URL = 'https://pmvnulxzytrukuenijtn.supabase.co';
-window.FOCUS_SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_gqDhjl9cNwQ5iflnTF11aQ_SXdU3IPf';
