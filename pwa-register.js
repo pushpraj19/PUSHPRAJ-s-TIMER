0dc12568a33659251c9ce491c9ac7e796b117709
pushpraj19 <pushprajbudha19@gmail.com>
2026-07-20 21:38:08 +0545

    Delete pwa-register.js

--- pwa-register.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js').catch(error => {
-      console.warn('Offline mode could not be enabled.', error);
-    });
-  });
-}
